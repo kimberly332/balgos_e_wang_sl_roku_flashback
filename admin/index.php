30c79c347b352ce252284cb38209add663c86f0e
kimberly332 <kimberly.wang1010@gmail.com>
2021-03-12 18:50:01 -0500

    update kids zone content

--- admin/index.php
+++ admin/index.php
@@ -30,7 +30,7 @@ confirm_logged_in();
             <input class="menu-btn" type="checkbox" id="menu-btn" />
             <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
             <ul class="menu">
-              <li><a href="#">Kids</a></li>
+              <li><a href="../kids.html">Kids</a></li>
               <li><a href="#">Movies</a></li>
               <li><a href="#">TV Show</a></li>
               <li><a href="#">Account</a></li>

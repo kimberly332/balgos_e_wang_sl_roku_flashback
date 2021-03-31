b43bb483cfb2432dff83f64860226eea2cff5acd
kimberly332 <kimberly.wang1010@gmail.com>
2021-03-31 04:37:24 -0400

    updating latest version

--- admin/index.php
+++ admin/index.php
@@ -32,7 +32,7 @@ confirm_logged_in();
             <ul class="menu">
               <li><a href="../kids.html">Kids</a></li>
               <li><a href="#">Movies</a></li>
-              <li><a href="#">Account</a></li>
+              <li><a href="account.html">Account</a></li>
               <li><a href="admin_logout.php">Sign Out</a></li>
             </ul>
         </header>
